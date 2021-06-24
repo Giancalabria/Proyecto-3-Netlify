0b7d181d4576cf7e97d2ad552f59dccbb68d58f9
Giancalabria <80368451+Giancalabria@users.noreply.github.com>
2021-06-23 21:34:36 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './utils/styles/global.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from './app'
+import { App } from './App'
 import { ThemeProvider } from './utils/styles/theme-context.js'
 ReactDOM.render(
 	<React.StrictMode>
