97b457742ceb048000b65a20b753c70da860bc0b
Giancalabria <80368451+Giancalabria@users.noreply.github.com>
2021-06-24 21:41:12 -0300

    commit

---
+++ src/components/main/filter/delete/delete.js
@@ -0,0 +1,28 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../../../../utils/styles/theme-context'
+import styles from './delete.module.scss'
+import PropTypes from 'prop-types'
+import { ReactComponent as Tacho } from '../../../../assets/svg/tacho.svg'
+export const Delete = ({ setSearch, setIsSearching }) => {
+	const { darkMode } = useContext(ThemeContext)
+	return (
+		<button
+			className={`${darkMode ? styles.darkButton : styles.lightButton}  ${
+				styles.button
+			}`}
+			onClick={() => {
+				setSearch('')
+				setIsSearching(true)
+			}}
+		>
+			<Tacho
+				className={`${darkMode ? styles.dark : styles.light}  ${styles.svg}`}
+			/>
+		</button>
+	)
+}
+
+Delete.propTypes = {
+	setSearch: PropTypes.func,
+	setIsSearching: PropTypes.func,
+}
